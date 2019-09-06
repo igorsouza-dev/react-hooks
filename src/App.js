25c810e16c76f1d7cbe87c0a33c1f9c3d9277290
Igor Souza <igorsouza.dev@gmail.com>
2019-09-05 21:05:08 -0300

    Testing useState

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function App() {
-  return <h1>Hello World</h1>;
+  const [tech, setTech] = useState(['React', 'Reactjs', 'ReactNative']);
+  const [newTech, setNewTech] = useState('');
+  function handleAdd() {
+    setTech([...tech, newTech]);
+  }
+  return (
+    <>
+      <ul>
+        {tech.map(t => (
+          <li key={t}>{t}</li>
+        ))}
+      </ul>
+      <input
+        type="text"
+        value={newTech}
+        onChange={e => setNewTech(e.target.value)}
+        placeholder="Type the new tech"
+      />
+      <button onClick={() => handleAdd()}>Add</button>
+    </>
+  );
 }
 
 export default App;
